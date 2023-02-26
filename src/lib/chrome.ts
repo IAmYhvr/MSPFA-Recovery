3d945132e972426de787464226cacdd5c88da696
Yhvr <yhvr@protonmail.com>
2023-02-26 10:47:36 -0500

    move unused code to separate folder
    I'm so disappointed in myself for the chromecache code

--- src/lib/chrome.ts
+++ src/lib/chrome.ts
@@ -1,9 +1,9 @@
 import { getSQL } from "./sql";
-import {
-	acquireFruitsOfYourLabor,
-	readCacheFile,
-	setCacheReadFn,
-} from "./readCacheFile";
+// import {
+// 	acquireFruitsOfYourLabor,
+// 	readCacheFile,
+// 	setCacheReadFn,
+// } from "./readCacheFile";
 
 const store = {};
 const MAINTENANCE_START = 1675433420000;
@@ -43,87 +43,29 @@ export async function processDB(file: File) {
 	return transformStore(store);
 }
 
-let theRes;
-let cacheGoal = 0;
-let cacheDone = 0;
-let queue: FileSystemFileEntry[] = [];
-const cacheRead = () => {
-	cacheDone++;
-	// @ts-expect-error
-	window.progressUpdate(cacheDone, cacheGoal);
-	if (cacheGoal === cacheDone) return allDone();
-	// setTimeout(tickQueue, 100);
-	tickQueue();
-};
-setCacheReadFn(cacheRead);
-function tickQueue() {
-	let entry = queue[0];
-	queue.shift();
-	entry.file(readCacheFile);
-}
-function allDone() {
-	theRes(transformStore(store));
-}
-function recurseReader(
-	reader: FileSystemDirectoryReader,
-	entries: FileSystemEntry[] = []
-) {
-	reader.readEntries(newEntries => {
-		if (newEntries.length > 0)
-			recurseReader(reader, entries.concat(newEntries));
-		else {
-			console.log(entries.length);
-			entries.forEach(entry => {
-				if (!entry.isFile) return;
-				cacheGoal++;
-				// @ts-expect-error
-				queue.push(entry);
-			});
-
-			cacheRead();
-		}
-	});
-}
-
-export function traverseFuckingTree(Cache: FileSystemDirectoryEntry) {
-	// Weird folder names to match the folder names on disk
-	// It just works for me, sorry lol
-	Cache.getDirectory(
-		"Cache_Data",
-		{},
-		(Cache_Data: FileSystemDirectoryEntry) => {
-			recurseReader(Cache_Data.createReader());
-		}
-	);
-
-	return new Promise(res => {
-		theRes = res;
-	});
-}
 
 function transformStore(store) {
 	let out = "Chrome\n";
-	const { adventures, users, css, js } = acquireFruitsOfYourLabor();
-	for (const id in adventures) {
-		out += `Adventure #${id}: ${JSON.stringify(adventures[id])}\n`;
-	}
+	// const { adventures, users, css, js } = acquireFruitsOfYourLabor();
+	// for (const id in adventures) {
+	// 	out += `Adventure #${id}: ${JSON.stringify(adventures[id])}\n`;
+	// }
 
 	for (const id in store) {
-		if (!(id in adventures))
-			out += `Adventure #${id}: ${JSON.stringify(store[id])}\n`;
+		out += `Adventure #${id}: ${JSON.stringify(store[id])}\n`;
 	}
 
-	for (const id in css) {
-		out += `Style #${id}: ${JSON.stringify(css[id])}\n`;
-	}
+	// for (const id in css) {
+	// 	out += `Style #${id}: ${JSON.stringify(css[id])}\n`;
+	// }
 
-	for (const id in js) {
-		out += `Script #${id}: ${JSON.stringify(js[id])}\n`;
-	}
+	// for (const id in js) {
+	// 	out += `Script #${id}: ${JSON.stringify(js[id])}\n`;
+	// }
 
-	for (const id in users) {
-		out += `User #${id}: ${JSON.stringify(users[id])}\n`;
-	}
+	// for (const id in users) {
+	// 	out += `User #${id}: ${JSON.stringify(users[id])}\n`;
+	// }
 
 	return out;
 }
