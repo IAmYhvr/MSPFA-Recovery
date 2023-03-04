1e83dc29aa2372afb5a92745bb6b5a83a8ac0911
Grant Gryczan <grantgryczan@gmail.com>
2023-03-03 19:31:03 -0500

    Fix FAQ

--- components/pages/FAQ.tsx
+++ components/pages/FAQ.tsx
@@ -4,20 +4,20 @@ import useLinkTo from 'lib/useLinkTo';
 
 export default function FAQ() {
 	return (
-		<main style={{ maxWidth: '850px' }}>
+		<main id="faq" style={{ maxWidth: '850px' }}>
 			<h1>FAQ</h1>
 
-			<h3>Are my adventures safe?</h3>
+			<h2>Are my adventures safe?</h2>
 			<p>
 				Yes.
 			</p>
 
-			<h3>Are you sure?</h3>
+			<h2>Are you sure?</h2>
 			<p>
 				Yes, no page data was ever lost in the first place. In fact, all adventures' pages have been backed up in multiple different locations to be extra safe.
 			</p>
 
-			<h3>What happened, and how can I be sure it won't happen again?</h3>
+			<h2>What happened, and how can I be sure it won't happen again?</h2>
 			<ul>
 				<li>Some recent metadata was lost (see next FAQ) due to backups not running properly during an unexpected database failure on February 3.</li>
 				<li>The sysadmin whose responsibility it was to manage backups continually delayed fixing the backup system, despite being frequently notified of the issue as soon as it was discovered. Eventually, he stopped responding entirely. He's no longer on our team.</li>
@@ -25,7 +25,7 @@ export default function FAQ() {
 				<li>Even if you don't trust our backup system, we'll add a new option to download a copy of all your adventure's data once the site's back.</li>
 			</ul>
 
-			<h3>What data was lost?</h3>
+			<h2>What data was lost?</h2>
 			<ul>
 				<li>For a large percentage of adventures and users, we've already recovered more recent data from the below losses!</li>
 				<li>Some adventure favorites, ownerships, descriptions, icons, or tags created in the 6 months before the incident were lost (since August 11, 2022).</li>
@@ -33,14 +33,14 @@ export default function FAQ() {
 				<li>All pages, comments, and any data before the aforementioned dates are 100% safe and sound.</li>
 			</ul>
 
-			<h3>Will I have to prove ownership of my adventure?</h3>
+			<h2>Will I have to prove ownership of my adventure?</h2>
 			<p>
 				While some adventure ownerships might need to be proven manually, many can be proven and restored completely automatically. Sit tight for now since we dont want people to go through the likely unnecessary effort of proving anything until we've already done everything automatic that we can.
 			</p>
 
-			<h3>When will the site be back?</h3>
+			<h2>When will the site be back?</h2>
 			<p>
-				No clue, but rest assured we're working our asses off. It'll most likely be a couple weeks. There's a to-do list actively updated with our progress on the <a href="https://mspfa.com/discord" target="_blank" rel="noreferrer">MSPFA Discord server</a>, pinned to the #mspfa-recovery channel.
+				No clue, but rest assured we're working our asses off. It'll most likely be a couple weeks. There's a to-do list actively updated with our progress in the <a href="https://mspfa.com/discord" target="_blank" rel="noreferrer">MSPFA Discord server</a>, pinned to the #mspfa-recovery channel.
 			</p>
 
 			<p>
