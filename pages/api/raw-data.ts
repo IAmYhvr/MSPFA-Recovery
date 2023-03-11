a1c1403ec0cc4b9bb72c3d1e60e2b35efba90601
Grant Gryczan <grantgryczan@gmail.com>
2023-03-11 05:19:56 -0500

    Add raw data API

---
+++ pages/api/raw-data.ts
@@ -0,0 +1,65 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import zlib from 'node:zlib';
+import { once } from 'node:events';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export const config = {
+	api: {
+		bodyParser: false
+	}
+};
+
+const ALLOWED_ORIGINS = new Set(['https://search.google.com']);
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const { origin } = req.headers;
+
+	if (origin && ALLOWED_ORIGINS.has(origin)) {
+		res.setHeader('Access-Control-Allow-Origin', origin);
+		res.setHeader('Access-Control-Allow-Methods', 'POST');
+		res.setHeader('Access-Control-Allow-Headers', '*');
+	}
+
+	if (req.method === 'OPTIONS') {
+		res.setHeader('Allow', 'OPTIONS, POST').end();
+		return;
+	}
+
+	if (req.method !== 'POST') {
+		res.status(405).end();
+		return;
+	}
+
+	let url;
+	try {
+		url = new URL(String(req.headers['mspfa-url']));
+	} catch {
+		res.status(500).end();
+		return;
+	}
+
+	if (url.hostname !== 'mspfa.com') {
+		res.status(500).end();
+		return;
+	}
+
+
+	const date = new Date();
+	const dateString = `${date.getFullYear()}-${`0${date.getMonth() + 1}`.slice(-2)}-${`0${date.getDate()}`.slice(-2)}`;
+
+	const parentPath = path.join(process.cwd(), '..', 'raw-data', dateString);
+	await fs.promises.mkdir(parentPath, { recursive: true });
+
+	const id = encodeURIComponent(url.pathname + url.search).replace(/%/g, '_');
+	const filename = `${id}.html.gz`;
+	const filePath = path.join(parentPath, filename);
+
+	const gzipCompress = zlib.createGzip();
+	const writeStream = fs.createWriteStream(filePath);
+
+	req.pipe(gzipCompress).pipe(writeStream);
+
+	await once(writeStream, 'close');
+	res.end();
+}
