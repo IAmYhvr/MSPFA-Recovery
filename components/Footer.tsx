74efdb3fe5da0a0ca65a1c5ccff6e57a6555caf4
Grant Gryczan <grantgryczan@gmail.com>
2023-05-09 21:08:11 -0400

    Update site to reflect post-recovery

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,6 @@ export default function Footer() {
 			<a href="https://mspfa.com" target="_blank" rel="noreferrer">MSPFA</a>
 			{' | '}
 			<a href="https://mspfa.com/discord" target="_blank" rel="noreferrer">Discord</a>
-			{' | '}
-			<a href="https://github.com/YhvrWasTaken/MSPFA-Recovery" target="_blank" rel="noreferrer">Source Code</a>
 		</footer>
 	);
 }
--- components/pages/FAQ.tsx
+++ components/pages/FAQ.tsx
@@ -5,43 +5,34 @@ export default function FAQ() {
 
 			<h2>Are my adventures safe?</h2>
 			<p>
-				Yes.
-			</p>
-
-			<h2>Are you sure?</h2>
-			<p>
-				Yes, no page data was ever lost in the first place. In fact, all adventures' pages have been backed up in multiple different locations to be extra safe.
+				Yes, no page data was ever lost in the first place. Keep reading!
 			</p>
 
 			<h2>What happened, and how can I be sure it won't happen again?</h2>
 			<ul>
-				<li>Some recent metadata was lost (see next FAQ) due to backups not running properly during an unexpected database failure on February 3.</li>
+				<li>MSPFA suffered a minor data loss due to backups not running properly during an unexpected database failure on February 3, 2023.</li>
 				<li>The sysadmin responsible for managing backups continually delayed fixing the backup system, despite being frequently notified of the issue as soon as it was discovered. Eventually, he stopped responding entirely. He's no longer on our team.</li>
-				<li>The site's current owner will fix the backup system himself and personally ensure it's 100% working before putting the site online again.</li>
-				<li>Even if you don't trust our backup system, we'll add a new option to download a copy of all your adventure's data once the site's back.</li>
+				<li>Our new backup system publicly reports details on each successful backup to a daily log in our <a href="https://mspfa.com/discord" target="_blank" rel="noreferrer">Discord server</a>, so from now on, you can see for yourself whether our backups are working!</li>
+				<li>We also added an option to export a copy of your adventure's data at the bottom of the page editor.</li>
 			</ul>
 
 			<h2>What data was lost?</h2>
 			<ul>
-				<li>For a large percentage of adventures and users, we've already recovered more recent data from the below losses!</li>
-				<li>Some adventure favorites, ownerships, descriptions, icons, or tags created in the 6 months before the incident were lost (since August 11, 2022).</li>
-				<li>Some usernames, profile icons, descriptions, and messages created in the 7 months before the incident were lost (since June 26, 2022).</li>
-				<li>All pages and comments, as well as any data before the aforementioned dates are 100% safe and sound.</li>
+				<li><strong>PLEASE: Re-favorite any adventures you favorited</strong> between August 11, 2022 and February 3, 2023! Favorites were the biggest loss, and if I were an author, I'd hate to lose a bunch of those. So help an author out and re-favorite anything you can. Spread the word!</li>
+				<li>Some other adventure metadata (NOT pages or comments) in that time frame was also lost, such as ownerships and changes to titles, descriptions, icons, tags, CSS, etc. We've recovered the majority of that, but <strong>you may want to double-check your adventures' info to be sure.</strong></li>
+				<li>Some user metadata since June 26, 2022 was lost, such as changes to settings, profile info, and game saves, but we've recovered most profile info. <strong>Double-check your settings and profile too!</strong></li>
+				<li>All pages and comments are 100% safe and sound, as well as any data before the aforementioned dates.</li>
 			</ul>
 
-			<h2>Will I have to prove ownership of my adventure?</h2>
+			<h2>I lost ownership of my adventure! What do I do?</h2>
 			<p>
-				While some adventure ownerships might need to be proven manually, many can be proven and restored completely automatically. Sit tight for now since we dont want people to go through the likely unnecessary effort of proving anything until we've already done everything automatic that we can.
+				Visit the #reclaim-your-adventure channel in the <a href="https://mspfa.com/discord" target="_blank" rel="noreferrer">MSPFA Discord server</a>, and we can help you out!
 			</p>
-
-			<h2>When will the site be back?</h2>
 			<p>
-				No clue, but rest assured we're working our asses off. It'll most likely be another week or two. There's a to-do list actively updated with our progress in the <a href="https://mspfa.com/discord" target="_blank" rel="noreferrer">MSPFA Discord server</a>, pinned to the #mspfa-recovery channel.
+				<strong>Even if you don't care about an adventure you lost ownership of</strong>, <em>please</em> still make a post there. We want there to be as few adventures with missing owners as possible--it helps our data management, archival, and everyone's user experience.
 			</p>
-
-			<h2>How can I help?</h2>
 			<p>
-				This website used to host a tool you could use to help recover some data, but we've collected enough and no longer have the tool available. Thanks everyone who submitted data to help us recover!
+				If you aren't able to use Discord, you can also contact us via email at <a href="mailto:support@mspfa.com" target="_blank">support@mspfa.com</a>, but our response time may be slower due to the asynchronous nature of email. (Also, we're still repairing our email system, so you'll have to wait a few days before we're able to send replies.)
 			</p>
 		</main>
 	);
